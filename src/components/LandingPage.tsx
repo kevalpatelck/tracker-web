22cac5ec897b78938c6c44c1064b3d3606628a39
Kishan <femil.coderkube@gmail.com>
2025-09-17 17:39:43 +0530

    changes

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -14,6 +14,7 @@ import {
   DollarSign,
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import imglogo from "../assets/img/Group1.jpeg";
 
 const LandingPage: React.FC<any> = () => {
   const navigate = useNavigate();
